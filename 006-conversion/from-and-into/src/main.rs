a0775681e612cdefd2852cc689629bf4a997229f
Yoga Hapriana <thenelse.v2@gmail.com>
2023-11-30 15:14:25 +0700

    Add From and Into

---
+++ 006-conversion/from-and-into/src/main.rs
@@ -0,0 +1,28 @@
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+// impl Into<Number> for i32 {
+//     fn into(self) -> Number {
+//         Number { value: self }
+//     }
+// }
+
+fn main() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    let int = 5;
+    // Try removing the type annotation
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}
